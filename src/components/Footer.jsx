d87acda0bd3f1958a860b751aac1b0f8b67117ec
Divvu1103 <200110075@hbtu.ac.in>
2023-02-26 14:50:13 +0530

    footer editing done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="text-white body-font bg-[#00142A]">
-      <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+      <div className="container px-5 py-24  flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
         <div className="w-1/4 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
           <span className="ml-3 text-lg">
             SHRI BABU SINGH DADDU JI AYURVEDIC MEDICAL COLLEGE & HOSPITAL
@@ -26,7 +26,7 @@ const Footer = () => {
             </p>
           </div>
         </div>
-        <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+        <div className=" flex flex-wrap md: -mb-10 md:mt-0 mt-10 md:text-left text-center">
           <div className="lg:w-2/6 md:w-1/2 w-full px-4">
             <h2 className="title-font  text-white tracking-widest text-2xl font-bold mb-3">
               Contact
@@ -48,124 +48,70 @@ const Footer = () => {
               <li>
                 <div className="text-white ">
                   <i class="fa-regular fa-envelope text-2xl text-[#014A83] -ml-5"></i>
-                  <div className="ml-4 font-bold text-xl">
+                  <div className="font-bold text-xl -ml-10">
                     sbsdaddujiaydcollege@gmail.com
                   </div>{" "}
                 </div>
               </li>
             </nav>
           </div>
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
-              CATEGORIES
+          <div className="lg:w-1/3 md:w-1/2 w-full px-4">
+            <h2 className="title-font  text-white tracking-widest text-2xl font-bold mb-3">
+              Quick Links
             </h2>
-            <nav className="list-none mb-10">
-              <li>
-                <a className="text-white ">First Link</a>
-              </li>
-              <li>
-                <a className="text-white ">Second Link</a>
-              </li>
-              <li>
-                <a className="text-white ">Third Link</a>
-              </li>
-              <li>
-                <a className="text-white ">Fourth Link</a>
-              </li>
-            </nav>
+            <div className="flex flex-row mb-10 gap-x-2">
+              <div className="flex flex-col">
+                <a className="text-white text-base font-bold">
+                  Overview SBSDJAMC
+                </a>
+                <a className="text-white text-base font-bold">
+                  {" "}
+                  Mission & Vision
+                </a>
+                <a className="text-white text-base font-bold">
+                  {" "}
+                  Fountained Inspiration
+                </a>
+                <a className="text-white text-base font-bold"> Courses</a>
+                <a className="text-white text-base font-bold"> Departments</a>
+              </div>
+              <div className="flex flex-col ">
+                <a className="text-white text-base font-bold">
+                  NCISM Mandatory Disclouser
+                </a>
+                <a className="text-white text-base font-bold"> Contact Us</a>
+                <a className="text-white text-base font-bold"> Gallery</a>
+                <a className="text-white text-base font-bold"> Apply Online</a>
+                <a className="text-white text-base font-bold">
+                  {" "}
+                  Grievance Form
+                </a>
+              </div>
+            </div>
           </div>
           <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
-              CATEGORIES
+          <h2 className="title-font  text-white tracking-widest text-2xl font-bold mb-3">
+              Important Links
             </h2>
             <nav className="list-none mb-10">
               <li>
-                <a className="text-white ">First Link</a>
+                <a className="text-white  text-base font-bold ">www.ayush.gov.in</a>
               </li>
               <li>
-                <a className="text-white ">Second Link</a>
+                <a className="text-white  text-base font-bold ">www.ncismindia.orgk</a>
               </li>
               <li>
-                <a className="text-white ">Third Link</a>
+                <a className="text-white  text-base font-bold ">www.kanpuruniversity.org</a>
               </li>
               <li>
-                <a className="text-white ">Fourth Link</a>
+                <a className="text-white  text-base font-bold ">Fourth Link</a>
               </li>
             </nav>
           </div>
         </div>
       </div>
-      <div className="bg-gray-100">
-        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-          <p className="text-gray-500 text-sm text-center sm:text-left">
-            © 2020 Tailblocks —
-            <a
-              href="https://twitter.com/knyttneve"
-              rel="noopener noreferrer"
-              className="text-gray-600 ml-1"
-              target="_blank"
-            >
-              @knyttneve
-            </a>
-          </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-            <a className="text-gray-500">
-              <svg
-                fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </a>
-            <a className="ml-3 text-gray-500">
-              <svg
-                fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </a>
-            <a className="ml-3 text-gray-500">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-              </svg>
-            </a>
-            <a className="ml-3 text-gray-500">
-              <svg
-                fill="currentColor"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="0"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="none"
-                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                ></path>
-                <circle cx="4" cy="4" r="2" stroke="none"></circle>
-              </svg>
-            </a>
-          </span>
-        </div>
+      <div className="bg-bg-[#00142A] border-t-2 border-gray-500">
+        <p className="mx-auto text-white text-center py-5 "> Copyright © 2023 SBSDJ Ayurvedic Medical College & Hospital. All Rights Reserved.  Powered By SOM SOFTWARE INFOTECH</p>
       </div>
     </footer>
   );
