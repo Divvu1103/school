2e55836eac37f9a387e281dc177077abdf448f16
Divvu1103 <200110075@hbtu.ac.in>
2023-03-05 18:17:19 +0530

    update section changes

--- src/components/Updatepoint.jsx
+++ src/components/Updatepoint.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Updatepoint = ({ name }) => {
   return (
-    <div className="text-blue-700 p-2 font-semibold border-b-4 border-black">
-      <i class="fa-solid fa-arrow-right"></i>
+    <div className="text-blue-700 p-2 pb-3 font-semibold border-b-4 border-black">
+      <span className="font-extrabold text-blue-800 mx-2">
+        <i className="fa-solid fa-arrow-right "></i>
+      </span>
       {name}
     </div>
   );
