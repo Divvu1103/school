1f928ab6efeeee96c69200b80785ff8ff0eae020
Divvu1103 <200110075@hbtu.ac.in>
2023-03-15 00:26:55 +0530

    changes

--- src/components/Updatepoint.jsx
+++ src/components/Updatepoint.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Updatepoint = ({ name }) => {
   return (
-    <div className="text-[#fff000] w-11/12 sm:w-full  sm:p-2 sm:pb-3  font-medium sm:font-semibold border-b-4 border-[#fff000]">
-      <span className="font-extrabold text-[#fff000] mx-2">
+    <div className="text-blue-700  w-11/12 sm:w-full  sm:p-2 sm:pb-3  font-medium sm:font-semibold border-b-4 border-black">
+      <span className="font-extrabold text-blue-800 mx-2">
         <i className="fa-solid fa-arrow-right "></i>
       </span>
       {name}
